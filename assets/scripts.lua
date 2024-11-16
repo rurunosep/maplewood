b044c31c103d4f8061b920e2de727246b4985439
Kevin Hernandez-Rives <rurunosep@gmail.com>
2024-11-16 00:44:24 -0500

    Bakery girl bun scene

--- assets/scripts.lua
+++ assets/scripts.lua
@@ -68,20 +68,53 @@ local stages = {
   [1] = function()
     message("I sell buns.")
   end,
+
   [2] = function()
     message("You need a Super Sugar Bun?")
     message("That's 25 cents.")
     message("You can get a quarter by returning a shopping cart.")
     set_story_var("bakery::girl::stage", 3)
   end,
+
   [3] = function()
     message("Get the quarter.")
   end,
+
   [4] = function()
-    message("Okay, here's the bun.")
+    message("\"I'll get you your bun.\"")
+
+    set_cutscene_border()
+    lock_player_input()
+    remove_camera_target()
+
+    walk("CAMERA", "up", 4, 0.05)
+    walk_wait("bakery_girl", "up", 0.75, 0.08)
+    walk_wait("bakery_girl", "left", 8, 0.08)
+    walk_wait("bakery_girl", "up", 4.5, 0.08)
+    walk_wait("bakery_girl", "right", 6.5, 0.08)
+    walk_wait("bakery_girl", "up", 0, 0.08)
+    wait(1)
+    walk_wait("bakery_girl", "left", 6.5, 0.08)
+    walk_wait("bakery_girl", "down", 4.5, 0.08)
+    walk_wait("bakery_girl", "right", 8, 0.08)
+    walk_wait("bakery_girl", "down", 0.4, 0.08)
+    wait(0.5)
+    message("\"Here's your bun.\"")
+    wait(1)
+    set_entity_visible("bun_fire", true)
+    wait(1)
+    walk("CAMERA", "down", 4, 0.05)
+    wait(2)
+    message("\"Take care!\"")
+
+    set_camera_target("player")
+    unlock_player_input()
+    remove_cutscene_border()
+
     set_story_var("bakery::girl::stage", 5)
     set_story_var("gym::janitor::stage", 4)
   end,
+
   [5] = function()
     message("Have a nice day.")
   end
